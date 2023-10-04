63eda24327084ab3932868f07a77ad0173b53297
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-10-04 15:04:41 -0300

    variável e soma

---
+++ cap.2/codigo1.java
@@ -0,0 +1,16 @@
+public class codigo1 {
+    
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to java programming");
+
+        System.out.print("Welcome to ");
+        System.out.println("Java Programming!");
+        System.out.println("Welcome\nto\nJava\nProgramming!");
+
+        System.out.printf("%s%n%s%n",
+        "Welcome to", "Java Programming!");
+        //printf é uma formatação para gerar 2 linhas usando a virgula
+    }
+    
+}
