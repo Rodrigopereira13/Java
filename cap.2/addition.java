0d7c5b0c8e85e7b4edfdebde06aa9037f1cc762a
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-10-04 16:31:23 -0300

    if em java

--- cap.2/addition.java
+++ cap.2/addition.java
@@ -4,17 +4,17 @@ public class addition {
 
     public static void main(String[] args)
     {
-        Scanner input = new Scanner(System.in);
+        Scanner escreve = new Scanner(System.in);
 
         int number1;
         int number2;
         int sum;
 
         System.out.print("Enter first integer: ");
-        number1 = input.nextInt();
+        number1 = escreve.nextInt();
 
         System.out.print("Enter second integer: ");
-        number2  = input.nextInt();
+        number2  = escreve.nextInt();
 
         sum = number1 + number2;
 
