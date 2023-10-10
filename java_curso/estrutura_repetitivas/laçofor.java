fc6e484c13382e97e548320a190edf3c36625b52
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-10-10 17:04:38 -0300

    laço for

---
+++ java_curso/estrutura_repetitivas/laçofor.java
@@ -0,0 +1,21 @@
+package java_curso.estrutura_repetitivas;
+
+import java.util.Scanner;
+
+public class laçofor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int soma = 0;
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            soma = soma + x;
+        }
+        System.out.println(soma);
+
+        sc.close();
+
+    }
+
+}
