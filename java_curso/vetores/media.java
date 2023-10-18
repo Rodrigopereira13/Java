44213d268dfe0923e0534768c69ea30b0fe32494
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-10-18 10:34:08 -0300

    debugg

--- java_curso/vetores/media.java
+++ java_curso/vetores/media.java
@@ -8,6 +8,7 @@ public class media {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        System.out.println("digite os numeros de vetores");
         int n = sc.nextInt();
         double[] vect = new double[n];
         for (int i = 0; i < vect.length; i++) {
