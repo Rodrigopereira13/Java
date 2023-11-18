917c57ccb9492011c59ce1d99135aa2d4db26604
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-11-18 12:36:42 -0300

    criação de pasta

---
+++ java_curso/arquivos/Program5.java
@@ -0,0 +1,22 @@
+package java_curso.arquivos;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program5 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a file path: ");
+        String strPath = sc.nextLine();
+
+        File path = new File(strPath);
+
+        System.out.println("getName: " + path.getName());//o nome do arquivo no caminho
+        System.out.println("getParent: " + path.getParent());//o caminho do arquivo
+        System.out.println("getParent: " + path.getParent());//o caminho completo com arquivo.
+        
+        sc.close();
+    }
+}
