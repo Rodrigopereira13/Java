c7a5cec8e1d292498844442952e8e8b7601a731e
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-11-23 14:53:10 -0300

    generics

---
+++ java_curso/generics_set_get/Programs1/application/Program1.java
@@ -0,0 +1,28 @@
+package java_curso.generics_set_get.Programs1.application;
+
+import java.util.Scanner;
+
+import java_curso.generics_set_get.Programs1.services.PrintService;
+
+public class Program1 {
+    	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		PrintService<Integer> ps = new PrintService<>(); //para mudar o tipo para String mudar o <>
+		                                            //usando generics é melhor para reuso, type safety e permance
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+
+		ps.print();
+		Integer x = ps.first();
+		System.out.println("First: " + x);
+		
+		sc.close();
+	}
+}
---
+++ java_curso/generics_set_get/Programs1/services/PrintService.java
@@ -0,0 +1,30 @@
+package java_curso.generics_set_get.Programs1.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService<T> { //coloca na classe <> para falar que é generico
+    private List<T> list = new ArrayList<>(); //lista generica
+
+	public void addValue(T value) { //adicionar o valor na list
+		list.add(value);
+	}
+
+	public T first() {  //metodo first para retornar o primeiro elemento
+		if (list.isEmpty()) {
+			throw new IllegalStateException("List is empty");
+		}
+		return list.get(0);
+	}
+
+	public void print() {   //printar 
+		System.out.print("[");
+		if (!list.isEmpty()) {
+			System.out.print(list.get(0));
+		}
+		for (int i = 1; i < list.size(); i++) {
+			System.out.print(", " + list.get(i));
+		}
+		System.out.println("]");
+	}
+}
