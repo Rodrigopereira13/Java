3e213b03b19feef23c972293fd0304e8ee1a5e7c
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-11-27 16:35:19 -0300

    hashset e treeset

---
+++ java_curso/generics_set_get/ProgramTreeSet/application/ProgramTreeSet.java
@@ -0,0 +1,21 @@
+package java_curso.generics_set_get.ProgramTreeSet.application;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import java_curso.generics_set_get.ProgramTreeSet.entities.Product;
+
+public class ProgramTreeSet {
+    public static void main(String[] args) {
+        
+        Set<Product> set = new TreeSet<>();//no Tree set tem que implementar comparable na classe 
+        
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+        
+        for (Product p : set) {
+            System.out.println(p);
+        }
+    }
+}
