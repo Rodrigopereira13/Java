9092bb57b1dd45d315fe0872e8dc8806eda41917
Rodrigo Pereira <rodrigosantosp31@gmail.com>
2023-11-30 15:13:26 -0300

    Update UpperCaseName.java

--- java_curso/Cap14_funcional_lambda/P3Function/util/UpperCaseName.java
+++ java_curso/Cap14_funcional_lambda/P3Function/util/UpperCaseName.java
@@ -4,11 +4,11 @@ import java.util.function.Function;
 
 import java_curso.Cap14_funcional_lambda.P3Function.entities.Product;
 
-public class UpperCaseName implements Function<Product, String>{  //product é o argumento e String é nov resultado
+public class UpperCaseName implements Function<Product, String>{  //product é o argumento e String é a saida ou novo resultado
     //aceita um argumento do tipo especificado e retorna um valor do tipo especificado.
 
     @Override
-    public String apply(Product p) {
+    public String apply(Product p) { //apply function
         return p.getName().toUpperCase();
     }
     
